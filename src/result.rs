8973bd893153e861a40279b68810cde09198581e
Александр <sasha-kvasnikov@mail.ru>
2026-08-04 22:04:04 +0300

    feat: impl `std::error::Error` for cassette error type

--- src/result.rs
+++ src/result.rs
@@ -18,3 +18,18 @@ impl From<std::string::FromUtf8Error> for CassetteError {
         CassetteError::ParseOutputError
     }
 }
+
+impl std::fmt::Display for CassetteError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl std::error::Error for CassetteError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            CassetteError::Io(error) => Some(error),
+            _ => None,
+        }
+    }
+}
